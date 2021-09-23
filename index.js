493c7bd4885cf0b8b2dc201c4a00ceebd957d2ce
AloneSpace <43822593+AloneSpace@users.noreply.github.com>
2021-09-24 00:00:04 +0700

    Add thai char support

--- index.js
+++ index.js
@@ -6,19 +6,20 @@ const log = console.log
 const createPassword = require('./utils/createPassword')
 const savePassword = require('./utils/savePassword')
 
-program.version('1.0.0').description('Simple Password Generator')
+program.version('1.0.1').description('Simple Password Generator')
 
 program
   .option('-l, --length <number>', 'length of password', '8')
   .option('-s, --save', 'save password to passwords.txt')
   .option('-nn, --no-numbers', 'remove numbers')
   .option('-ns, --no-symbols', 'remove symbols')
+  .option('-th, --thai', 'add thai chars')
   .parse()
 
-const { length, save, numbers, symbols } = program.opts()
+const { length, save, numbers, symbols, thai } = program.opts()
 
 // Get generated password
-const generatedPassword = createPassword(length, numbers, symbols)
+const generatedPassword = createPassword(length, numbers, symbols, thai)
 
 // Save to file
 if (save) {
