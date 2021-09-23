5841a6cc81317f06bfc3a29c86202fd00b75e237
AloneSpace <43822593+AloneSpace@users.noreply.github.com>
2021-09-24 00:04:50 +0700

    Add thai alphabets

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,11 +1,13 @@
 const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+const thai_alpha = 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ'
 const numbers = '0123456789'
 const symbols = '!@#$%^&*_-+='
 
-const createPassword = (length = 8, hasNumbers = true, hasSymbols = true) => {
+const createPassword = (length = 8, hasNumbers = true, hasSymbols = true, hasThaiAlpha = false) => {
   let chars = alpha
   hasNumbers ? (chars += numbers) : ''
   hasSymbols ? (chars += symbols) : ''
+  hasThaiAlpha ? (chars += thai_alpha) : ''
   return generatePassword(length, chars)
 }
 
