38029fb22fdb0777be75c10b9596a40f91c5d5e2
AloneSpace <43822593+AloneSpace@users.noreply.github.com>
2021-09-24 00:37:04 +0700

    Update createPassword.js

--- utils/createPassword.js
+++ utils/createPassword.js
@@ -1,13 +1,16 @@
 const alpha = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-const thai_alpha = 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ๐๑๒๓๔๕๖๗๘๙'
+const thai_alpha = 'กขฃคฅฆงจฉชซฌญฎฏฐฑฒณดตถทธนบปผฝพฟภมยรลวศษสหฬอฮ'
+const thai_digit = '๐๑๒๓๔๕๖๗๘๙'
 const numbers = '0123456789'
 const symbols = '!@#$%^&*_-+='
 
-const createPassword = (length = 8, hasNumbers = true, hasSymbols = true, hasThaiAlpha = false) => {
+const createPassword = (length = 8, hasNumbers = true, hasSymbols = true, hasThaiChar = false, hasThaiAlpha = false, hasThaiDigit = false) => {
   let chars = alpha
   hasNumbers ? (chars += numbers) : ''
   hasSymbols ? (chars += symbols) : ''
+  hasThaiChar ? (chars += thai_alpha + thai_digit) : ''
   hasThaiAlpha ? (chars += thai_alpha) : ''
+  hasThaiDigit ? (chars += thai_digit) : ''
   return generatePassword(length, chars)
 }
 
